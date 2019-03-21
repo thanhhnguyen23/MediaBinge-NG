1a955b992bd3aa63f2f5446c147914d22558590b
Jaitee Pitts <jaiteepitts@gmail.com>
2019-03-21 19:04:06 -0400

    added basic styling to login and homepage

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mb-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
