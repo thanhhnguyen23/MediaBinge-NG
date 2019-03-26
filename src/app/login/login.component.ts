f7767732939eb6a778b7048b38ac756c8d9cfeb3
Thanh Nguyen <thanhhnguyen23@gmail.com>
2019-03-25 21:47:05 -0400

    login scaffolding contintues

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,17 +2,35 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router'; // adding login dependencies - TN
 
 import { LoginService } from '../login.service'; // adding login dependencies - TN
+import { Credentials } from '../models/credentials';// adding login dependencies - TN
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
 
-  ngOnInit() {
+  isAuthenticated$ = this.loginService.isAuthenticated;
+  credentialsInvalid: boolean = false;
+  credentials: Credentials;
+
+  constructor(private loginService: loginService, private router: Router) {
+    console.log('LoginComponent constructed');
   }
 
+  login(username: string, password: string): void{
+    this.credentials = new Credentials(username, password);
+    this.loginSerivce.authenticate(this.credentials);
+
+    this.isAuthenticated.subscribe(isAuth) => {
+      if(isAuth){
+        this.credentialsInvalid = false;
+        this.router.navigate(['services']);
+      }
+    }, err => {
+      this.credentialsInvalid = true;
+    }
+  }
 }
