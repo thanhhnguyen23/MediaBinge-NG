7c7e6a75deac3f26cdb97de25bd777062832c076
Thanh Nguyen <thanhhnguyen23@gmail.com>
2019-03-26 19:08:00 -0400

    revising buildspec try #6; fixing other dependencies

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,16 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
 import { Router } from '@angular/router'; // adding login dependencies - TN
-
 import { LoginService } from '../login.service'; // adding login dependencies - TN
 import { Credentials } from '../models/credentials';// adding login dependencies - TN
 
 @Component({
-  selector: 'app-login',
+  selector: 'mb-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+// @Component({
+//   selector: 'app-login',
+//   templateUrl: './login.component.html',
+//   styleUrls: ['./login.component.css']
+// })
 export class LoginComponent {
+
+  useremail:string;
+  userpassword:string;
+
+  loginUser(){
+      this.router.navigate(['profile']);
+  }
 
 
   isAuthenticated$ = this.loginService.isAuthenticated;
@@ -35,26 +45,5 @@ export class LoginComponent {
     // }, err => {
     //   this.credentialsInvalid = true;
     // });
-  }
-
-import { Router } from '@angular/router';
-
-@Component({
-  selector: 'mb-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
-})
-export class LoginComponent implements OnInit {
-
-  constructor(private router: Router) { }
-
-  useremail:string;
-  userpassword:string;
-
-  loginUser(){
-      this.router.navigate(['profile']);
-  }
-
-  ngOnInit() {
   }
 }
