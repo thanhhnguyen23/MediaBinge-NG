e732e499b4d8b69949a470a747e69c35f9388043
Jaitee Pitts <jaiteepitts@gmail.com>
2019-03-27 13:09:51 -0400

    trying to get observable to connect to local variable

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PostService } from '../services/PostService';
+import { Post } from 'src/models/Post';
 
 @Component({
   selector: 'mb-movies',
@@ -7,7 +8,13 @@ import { PostService } from '../services/PostService';
   styleUrls: ['./movies.component.css']
   // providers: [PostService]
 })
+
+
 export class MoviesComponent implements OnInit {
+  text: string;
+ 
+  current: Post;
+  datePost: string;
 
   constructor(private posts: PostService) { }
 
@@ -15,12 +22,65 @@ export class MoviesComponent implements OnInit {
     this.getPosts();
   }
   //get post from post service
-  text = "I have very strong opinions about movies";
-  getPosts() {
-    this.posts.getPosts().subscribe(function(data) {
-      console.log('posts', data);
-    });
+  allPost: Post[] = [
+    {
+      id: 4,
+      text: "wokka wokka wokka",
+      topic: "movie/tv",
+      userId: 3,
+      datePosted: '5/12/12 5:00pm'
+    },
+    {
+      id: 5,
+      text: "lalallalala",
+      topic: "movie/tv",
+      userId: 3,
+      datePosted: '5/12/12 6:00pm'
+    }
+  ];
 
-    
+  
+
+  createPosts(data){
+    //
   }
+
+  getPosts() {
+    this.posts.getPosts().subscribe(observer);
+      //function (data) {
+    //   console.log('posts', data);
+    //   let cards = document.getElementsByClassName('card-text');
+    //   cards[0].innerHTML = data[0].text;
+      // if (data[0] === null) {
+      //   this.text = "I have very strong opinions about movies";
+      // } else {
+      //   console.log(data[0]);
+      
+      //   this.text = data[0].text;
+        
+      // }
+      console.log(this.text);
+      
+    // });
+  }
+
+  
+  setPost(){
+    this.current = this.allPost[0];
+  }
+  
+  
+}
+
+let observer = {
+  next: function(data){
+    console.log('posts', data);
+    // this.allPost = data;
+    console.log("allPost" + this.allPost)
+    let cards = document.getElementsByClassName('card-text');
+    for(let i = 0; i < data.length; i++){
+      cards[i].innerHTML = data[i].text;
+      this.datePost = data[i].datePosted;
+    }
+  }    
 }
--- src/app/services/PostService.ts
+++ src/app/services/PostService.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Post } from 'src/models/Post';
-import { Response } from 'selenium-webdriver/http';
 
 @Injectable()
 export class PostService {
@@ -12,10 +11,21 @@ export class PostService {
     constructor(private httpClient: HttpClient) {
         console.log('PostService constructed!');
     }
+    postArray: Post[];
 
       getPosts(): Observable<Post> {
         return this.httpClient.get<Post>(this.postsUrl);
       }
-    
+//topic id
+//userid
+      observer = {
+        next: function(data){
+          // console.log('posts', data);
+          for(let i = 0; i < data.length; i++){
+            this.postArray[i] = data[i];
+          }
+        }    
+      }
+      
 
 }
